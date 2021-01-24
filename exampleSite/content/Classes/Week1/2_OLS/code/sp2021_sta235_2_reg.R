a443719a383177401501cd20aa68224ff97e6160
maibennett <mai.bennett@gmail.com>
2021-01-24 13:10:51 -0600

    upload code and data

---
+++ exampleSite/content/Classes/Week1/2_OLS/code/sp2021_sta235_2_reg.R
@@ -0,0 +1,11 @@
+##########################################################
+### Title: "Week 1 - Multiple Regression"
+### Course: STA 235
+### Semester: Spring 2021
+### Professor: Magdalena Bennett
+##########################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
