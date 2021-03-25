e5aa18ca6178dfce7158e723dce397ce74efeedf
maibennett <mai.bennett@gmail.com>
2021-03-25 12:58:09 -0500

    Create STA235_Midterm_RcodeExample.R

---
+++ exampleSite/content/Exams/code/STA235_Midterm_RcodeExample.R
@@ -0,0 +1,38 @@
+################################################################################
+### MIDTERM
+### Course: STA 235
+### Semester: Spring 2021
+### Name: [INSERT NAME HERE]
+################################################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
+
+### Load libraries
+# If you don't have one of these packages installed already, you will need to 
+#run install.packages() line
+# You might need more (or less) packages, depending on the task. 
+#You should load them here if necessary 
+library(tidyverse)
+library(ggplot2)
+library(estimatr)
+library(broom)
+library(rdrobust)
+
+# Remember to indicate the number of the question you are answering!
+
+################################################################################
+## Task 1
+
+
+################################################################################
+## Task 2
+
+
+
+################################################################################
+## Task 3
+
+
