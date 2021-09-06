29fa83daa4f5aeb68033b0893af6a4f9f7d42e79
maibennett <mai.bennett@gmail.com>
2021-09-06 18:43:06 -0500

    update week 2

--- exampleSite/content/Classes/Week2/2_OLS_probs/code/f2021_sta235h_4_reg_prob.R
+++ exampleSite/content/Classes/Week2/2_OLS_probs/code/f2021_sta235h_4_reg_prob.R
@@ -64,3 +64,70 @@ lm2 <- lm(price ~ rating + mileage + year*luxury, data = cars)
 summary(lm2)
 
 #Question: What's the change in price for one additional year for luxury-brand cars vs non-luxury-brand cars, holding other variables constant?
+
+
+## Let's plot mileage against year. What do you see?
+
+ggplot(data = cars, aes(y = mileage, x = year)) +
+  geom_point(fill = "white", color = "orange", size = 3, pch = 21) + #pch changes the type of the marker (you can see the options here: http://www.sthda.com/english/wiki/r-plot-pch-symbols-the-different-point-shapes-available-in-r)
+  theme_bw()+
+  xlab("Year") + ylab("Mileage") +
+  theme(axis.title.x = element_text(size=18),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=18),
+        axis.text.y = element_text(size=14),
+        legend.position="none",
+        title = element_text(size=18),
+        plot.margin=unit(c(1,1,1.5,1.2),"cm"),
+        panel.grid = element_blank(),
+        axis.line = element_line(colour = "dark grey"))
+
+
+# We can also easiy include a regression line using geom_smooth:
+ggplot(data = cars, aes(y = mileage, x = year)) +
+  geom_point(fill = "white", color = "orange", size = 3, pch = 21) + #pch changes the type of the marker (you can see the options here: http://www.sthda.com/english/wiki/r-plot-pch-symbols-the-different-point-shapes-available-in-r)
+  geom_smooth(method = "lm", color = "#900DA4", lwd = 1.5, se = FALSE) + #The method is `lm` (linear model), and we don't want to include the standard errors for the fitted line 
+  theme_bw()+
+  xlab("Year") + ylab("Mileage") +
+  theme(axis.title.x = element_text(size=18),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=18),
+        axis.text.y = element_text(size=14),
+        legend.position="none",
+        title = element_text(size=18),
+        plot.margin=unit(c(1,1,1.5,1.2),"cm"),
+        panel.grid = element_blank(),
+        axis.line = element_line(colour = "dark grey"))
+
+# Let's look at the correlation between year and mileage
+cars %>% select(year, mileage) %>% cor(.)
+
+
+# Now, let's look at it by luxury vs non-luxury car
+ggplot(data = cars, aes(y = mileage, x = year, color = factor(luxury))) + #Q: Why do we use factor(luxury) and not just luxury? Try it out without factor.
+  geom_point(size = 3, pch = 21) +
+  geom_smooth(method = "lm", lwd = 1.5, se = FALSE) + #The method is `lm` (linear model), and we don't want to include the standard errors for the fitted line 
+  scale_color_manual("Luxury",values=c("#E16462","#F89441")) + #with scale_color_manual we can choose the title of the legend and the colors for the different groups.
+  theme_bw()+
+  xlab("Year") + ylab("Mileage") + 
+  theme(axis.title.x = element_text(size=18),
+        axis.text.x = element_text(size=14),
+        axis.title.y = element_text(size=18),
+        axis.text.y = element_text(size=14),
+        legend.position=c(0.9,0.82),
+        title = element_text(size=12),
+        plot.margin=unit(c(1,1,1.5,1.2),"cm"),
+        panel.grid = element_blank(),
+        axis.line = element_line(colour = "dark grey"))
+
+# Looking at the previous plot, what correlation is greater (in absolute terms)?
+
+
+#### Let's build the residuals
+
+cars <- cars %>% mutate(price_hat = predict(lm(price ~ rating + mileage + year, data = .)), 
+                        residual = price - price_hat)
+
+
+# Looking at the code in class (and the code above), build a histogram for residuals and a catter plot for residuals and the dependent variable.
+# Note: Use "log(price)" for your dependent variable. How do the plots look if you use "price" instead?
--- exampleSite/content/Classes/Week2/_index.en.md
+++ exampleSite/content/Classes/Week2/_index.en.md
@@ -22,16 +22,16 @@ Complete before **Sunday Sep 5th (11:59 pm)** (if your section is on Tue) or **T
 
 ## Slides
 
-<!-- {{% button href="https://sta235.netlify.app/Classes/Week1/1_Intro/sp2021_sta235_1_intro.html" icon="fas fa-external-link-alt" icon-position="right" %}}New window{{% /button %}} {{% button href="https://sta235.netlify.app/Classes/Week1/1_Intro/sp2021_sta235_1_intro.pdf" icon="fas fa-file-pdf" icon-position="right" %}}Download{{% /button %}} 
+{{% button href="https://sta235.netlify.app/Classes/Week2/1_OLS/f2021_sta235h_3_reg.html" icon="fas fa-external-link-alt" icon-position="right" %}}New window{{% /button %}} {{% button href="https://sta235.netlify.app/Classes/Week2/1_OLS/f2021_sta235h_3_reg.pdf" icon="fas fa-file-pdf" icon-position="right" %}}Download{{% /button %}} 
 
-{{< slides src="https://sta235.netlify.app/Classes/Week1/1_Intro/sp2021_sta235_1_intro.html" >}}
+{{< slides src="https://sta235.netlify.app/Classes/Week2/1_OLS/f2021_sta235h_3_reg.html" >}}
 
 <br>
 
-{{% button href="https://sta235.netlify.app/Classes/Week1/2_OLS/sp2021_sta235_2_reg.html" icon="fas fa-external-link-alt" icon-position="right" %}}New window{{% /button %}} {{% button href="https://sta235.netlify.app/Classes/Week1/2_OLS/sp2021_sta235_2_reg.pdf" icon="fas fa-file-pdf" icon-position="right" %}}Download{{% /button %}} 
+{{% button href="https://sta235.netlify.app/Classes/Week2/2_OLS_probs/f2021_sta235h_4_reg.html" icon="fas fa-external-link-alt" icon-position="right" %}}New window{{% /button %}} {{% button href="https://sta235.netlify.app/Classes/Week2/2_OLS_probs/f2021_sta235h_4_reg.pdf" icon="fas fa-file-pdf" icon-position="right" %}}Download{{% /button %}} 
 
-{{< slides src="https://sta235.netlify.app/Classes/Week1/2_OLS/sp2021_sta235_2_reg.html" >}}  -->
+{{< slides src="https://sta235.netlify.app/Classes/Week2/2_OLS_probs/f2021_sta235h_4_reg.html" >}}
 
 ## Code
 
-<!-- Here is the R code we will review in class, with some additional data and questions <a onclick="ga('send', 'event', 'External-Link','click','code1','0','Link');" href="https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week1/2_OLS/code/sp2021_sta235_2_reg.R" target="_blank" class="btn btn-default">Download<i class="fas fa-code"></i></a> -->
+Here are the two R scripts we will review in class, with some additional data and questions <a onclick="ga('send', 'event', 'External-Link','click','code1','0','Link');" href="https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week1/2_OLS/code/f2021_sta235h_2_reg.R" target="_blank" class="btn btn-default">Download (Code Week 1)<i class="fas fa-code"></i></a> <a onclick="ga('send', 'event', 'External-Link','click','code1','0','Link');" href="https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week2/2_OLS_probs/code/f2021_sta235h_4_reg.R" target="_blank" class="btn btn-default">Download<i class="fas fa-code"></i></a>
