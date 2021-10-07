862bdddb3d0df64e3990237a2b71d966deb97462
Magdalena Bennett <mai.bennett@gmail.com>
2021-10-07 15:46:58 -0500

    update hw3

---
+++ exampleSite/content/Assignments/Homework/Homework3/STA235H_HW3_template.R
@@ -0,0 +1,34 @@
+######################################################################
+### Title: "Homework 3"
+### Course: STA 235H
+### Semester: Fall 2021
+### Students: Name 1, Name 2, Name 3, and Name 4
+#######################################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
+# scipen=999 removes scientific notation; scipen=0 turns it on.
+options(scipen = 0)
+
+### Load libraries
+# If you don't have one of these packages installed already, you will need to run install.packages() line
+library(tidyverse)
+library(modelsummary)
+library(ggplot2)
+library(estimatr)
+
+
+################################################################################
+### Task 1: Do it for the gram?
+################################################################################
+
+ig <- read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Assignments/Homework/data/hw3/instagram.csv")
+
+## Question 1.3
+
+
+#Complete this with your code
+
+#...
