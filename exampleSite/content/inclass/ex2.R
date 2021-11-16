a9cd49a0b3d8ef6c23da9a727c538b61bc4e4ec1
maibennett <mai.bennett@gmail.com>
2021-11-16 11:37:02 -0600

    update ex2

--- exampleSite/content/inclass/ex2.R
+++ exampleSite/content/inclass/ex2.R
@@ -25,8 +25,8 @@ library(modelr)
 d <- read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week12/1_DecisionTrees/data/Placement_Data_Full_Class.csv")
 
 # Task: We want to predict whether a student is placed or not (status) according to their characteristics.
-#       Run a KNN using 10-fold CV and whatever tuneLength you see fit. 
-#       What is the optimal K and the accuracy of the prediction?
+#       1) Run a Decision Tree using 10-fold CV and whatever tuneLength or TuneGrid you see fit. 
+#       2) Show a plot for your final tree. What is the optimal complexity parameter and the accuracy of the prediction?
 
 # Notes: `sl_no` is a serial number identifying students and should not be used for prediction. 
 #        `salary` is an outcome for those placed, so it should also be left out (not used as a predictor)
