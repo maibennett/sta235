9cbcc90ca2e91087f42c2cb2172dd34046b69bf3
maibennett <mai.bennett@gmail.com>
2021-11-19 13:29:57 -0600

    Update STA235H_SectionX_GroupY.R

--- exampleSite/content/Assignments/Project/STA235H_SectionX_GroupY.R
+++ exampleSite/content/Assignments/Project/STA235H_SectionX_GroupY.R
@@ -28,10 +28,13 @@ d <- d_total %>% slice(rows)
 # Now clean your data and conduct your analysis with the d dataset.
 
 ### REMEMBER TO DO EVERYTHING IN YOUR R SCRIPT
-# (When the instruction team runs this, they should get the same results using the original data)
+# (When the instruction team runs this, they should get the same results using 
+# the original data)
 
 #############################################
 ## EXAMPLE:
+#############################################
+
 # The purpose of this example is just to show the objects that you need to save. 
 # It is not an accurate depiction of a real model.
 
@@ -45,4 +48,7 @@ model2 <- train(business_or_commercial_purpose ~ interest_only_payment, data = d
              method = "lm")
 
 # Save your preferred model for binary outcome and continuous outcome
-save(lm1, lm2, file = "STA235H_SectionX_GroupY.Rdata") # You will need to submit this .Rdata file
+save(lm1, lm2, file = "STA235H_SectionX_GroupY.Rdata") 
+# You will need to submit this .Rdata file (if you used setwd(), it will be saved
+# in your working directory)
+
