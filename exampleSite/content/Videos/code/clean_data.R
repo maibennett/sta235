95d40da8924fa3bb2cd11b7a82feea10ecf2f978
maibennett <mai.bennett@gmail.com>
2022-08-31 19:00:36 -0500

    update

--- exampleSite/content/Videos/code/clean_data.R
+++ exampleSite/content/Videos/code/clean_data.R
@@ -9,22 +9,22 @@ data  = data.frame(gender = c(rep("F",5), rep("M",3)),
                    married = sample(c(0,1), 8, replace = TRUE, prob = c(0.65,0.35)))
 
 # Create a dataset with only married females:
-data_mf = data %>% filter(married == 1 & gender == "F")
 
-# Married people with income > 40,000
-data_m40 = data %>% filter(married == 1 & income>40000)
+data_fm = data %>% filter(married == 1 & gender == "F")
+
+# People with income > 40,000 OR not married
+
+data_i40s = data %>% filter(income > 40000 | married != 1)
 
 # Generate an indicator variable that captures people with income over 100,000 and under:
 
-ifelse(data$income > 100000, 1, 0)
-
 data = data %>% mutate(income100 = ifelse(income > 100000, 1, 0))
 
-data
+data  
 
-data = data %>% mutate(income100 = income > 100000)
+data = data %>% mutate(income100 = income > 100000)  
 
-data 
+data  
 
 data = data %>% mutate(income100 = as.numeric(income > 100000))
 
