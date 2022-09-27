7ee35fbb291bf80b2702686284e9a6daea3cc70e
Magdalena Bennett <mai.bennett@gmail.com>
2022-09-26 20:59:52 -0500

    Update f2022_sta235h_6_RCT.R

--- exampleSite/content/Classes/Week6/1_RCT/code/f2022_sta235h_6_RCT.R
+++ exampleSite/content/Classes/Week6/1_RCT/code/f2022_sta235h_6_RCT.R
@@ -26,44 +26,59 @@ library(modelsummary)
 # If you're running this live in class, load this dataset:
 d <- read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week6/1_RCT/data/sample_gotv.csv")
 
+# Drop this first variable (row names)
+d <- d %>% select(-X)
+
 # Some variables of interest
 
 # persons: Number of voters in the household (1 or 2)
-# competiv: Wether the district is competitive or not
-# vote*: Whether the person voted in year *
+# competiv: Wether the district is competitive or not (1: No, 2: Yes)
+# vote*: Whether the person voted in year * (vote00 and vote98 were before the randomization, vote02 was after)
 # newreg: Whether the person is newly registered as a voter or not
 # age: Age of the person
 # contact: Whether the household was contacted (1) or not (0)
 # treat_real: Whether the household was randomized into treatment (1) or not (0). If NA, the household was not part of the randomization sample.
-# state: Variable for the state.
+# state: Variable for the state (0 and 1).
 # female2: Whether the person is female or not.
 
 # Drop variables with unlisted phone numbers
 d_s1 <- d %>% filter(!is.na(treat_real))
 
-# 1) Check for balance
+####### 1. Check for balance
 
-# For each strata (they should be comparable within strata)
+# Create a dataset to check for balance. Remember to check for balance for each strata (they should be comparable within strata)
 
-d_s1_bal <- d_s1 %>% mutate(treat_strata = interaction(factor(treat_real),factor(competiv),factor(state))) %>%
-  select(-c(vote02, competiv, state, treat_real, treat_pseudo))
+# Transform competiv into a binary variable, and create two variables for state:
+d_s1 <- d_s1 %>% mutate(competiv = competiv - 1,
+                        stateA = ifelse(state==1, 1, 0))
 
-datasummary_balance(~ treat_strata, data = d_s1_bal, dinm_statistic = "p.value")
+# Create strata variables: Interaction between state and competitiveness level.
+# Also, we will create the interaction between state, competitiveness level, AND treatment level
+d_s1 <- d_s1 %>% mutate(strata = interaction(stateA, competiv),
+                        strata_treat = interaction(treat_real, stateA, competiv))
+
+# Create a dataset for balance:
+d_s1_bal <- d_s1 %>% select(strata_treat, persons, vote00, vote98, newreg, age, female2, fem_miss)
+
+datasummary_balance(~ strata_treat, data = d_s1_bal, dinm_statistic = "p.value")
 # Q: What do the column names mean? (e.g. what is 0.1.0 mean?)
 
-# Now let's look at the balance without considering strata. Should we see imbalances here?
-d_s1_bal2 <- d_s1 %>% select(-c(vote02, competiv, state, treat_pseudo))
+# What if we wanted to see p-values *within strata?*
 
-datasummary_balance(~ treat_real, data = d_s1_bal2, dinm_statistic = "p.value")
-# Q: What do the column names mean? (e.g. what is 0.1.0 mean?)
+# 1) See which strata do I have?
+d_s1 %>% select(strata) %>% table()
 
-# 2) Let's estimate the effect
+# 2) Create a dataset for each stratum
+d_s1_bal_s1 <- d_s1 %>% filter(strata=="0.0") %>% select(treat_real, persons, vote00, vote98, newreg, age, female2, fem_miss)
 
-library(estimatr) # We are using lm_robust() because we have a binary variable as an outcome.
+# 3) Create the balance table for the stratum
+datasummary_balance(~ treat_real, data = d_s1_bal_s1, dinm_statistic = "p.value", fmt=3)
+#Q: Can you do this for other strata?
 
-d_s1 <- d_s1 %>% mutate(strata = interaction(state, competiv))
 
-summary(lm_robust(vote02 ~ treat_real + strata, data = d_s1))
+####### 2. Estimate the causal effect
+
+summary(lm_robust(vote02 ~ treat_real + strata, data = d_s1)) # We are using lm_robust() because we have a binary variable as an outcome.
 
 # Q: What can you say about this evidence? Interpret the coefficient of interest.
 
@@ -75,7 +90,7 @@ summary(lm_robust(vote02 ~ treat_real + strata + vote00 + vote98 + age + female2
 #############################################
 
 # Just for performance purposes, we will use a smaller sample of the original data (just looking at one stratum):
-d_sample <- d_s1 %>% filter(state == 1 & competiv == 2)
+d_sample <- d_s1 %>% filter(state == 1 & competiv == 1)
 
 ## In this section, we are going to be working with observational data, meaning that there is no random assignment.
 ## In this case, then, the treatment variable will be "contact", and not treat_real. Imagine a firm that is just calling a bunch of numbers
@@ -124,7 +139,7 @@ datasummary_balance(~ contact, data = d_m1_bal, fmt = 3, dinm_statistic = "p.val
 # Q: What about balance for the unmatched sample, d_sample? How does that look?
 
 # Let's now run the simple regression of our outcome on our treatment variable:
-summary(estimatr::lm_robust(vote02 ~ contact, data = d_m1))
+summary(lm_robust(vote02 ~ contact, data = d_m1))
 
 #Q: Interpret the results. What do we find here?
 
