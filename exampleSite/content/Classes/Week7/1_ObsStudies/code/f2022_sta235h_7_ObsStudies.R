ac04be6f743a37c708f8bca42f091dd2ef3aeda9
Magdalena Bennett <mai.bennett@gmail.com>
2022-10-03 20:03:07 -0500

    update

--- exampleSite/content/Classes/Week7/1_ObsStudies/code/f2022_sta235h_7_ObsStudies.R
+++ exampleSite/content/Classes/Week7/1_ObsStudies/code/f2022_sta235h_7_ObsStudies.R
@@ -44,49 +44,6 @@ d <- d %>% select(-X)
 # Drop variables with unlisted phone numbers
 d_s1 <- d %>% filter(!is.na(treat_real))
 
-####### 1. Check for balance
-
-# Create a dataset to check for balance. Remember to check for balance for each strata (they should be comparable within strata)
-
-# Transform competiv into a binary variable, and create two variables for state:
-d_s1 <- d_s1 %>% mutate(competiv = competiv - 1,
-                        stateA = ifelse(state==1, 1, 0))
-
-# Create strata variables: Interaction between state and competitiveness level.
-# Also, we will create the interaction between state, competitiveness level, AND treatment level
-d_s1 <- d_s1 %>% mutate(strata = interaction(stateA, competiv),
-                        strata_treat = interaction(treat_real, stateA, competiv))
-
-# Create a dataset for balance:
-d_s1_bal <- d_s1 %>% select(strata_treat, persons, vote00, vote98, newreg, age, female2, fem_miss)
-
-datasummary_balance(~ strata_treat, data = d_s1_bal, dinm_statistic = "p.value")
-# Q: What do the column names mean? (e.g. what is 0.1.0 mean?)
-
-# What if we wanted to see p-values *within strata?*
-
-# 1) See which strata do I have?
-d_s1 %>% select(strata) %>% table()
-
-# 2) Create a dataset for each stratum
-d_s1_bal_s1 <- d_s1 %>% filter(strata=="0.0") %>% select(treat_real, persons, vote00, vote98, newreg, age, female2, fem_miss)
-
-# 3) Create the balance table for the stratum
-datasummary_balance(~ treat_real, data = d_s1_bal_s1, dinm_statistic = "p.value", fmt=3)
-#Q: Can you do this for other strata?
-
-
-####### 2. Estimate the causal effect
-
-summary(lm_robust(vote02 ~ treat_real + strata, data = d_s1)) # We are using lm_robust() because we have a binary variable as an outcome.
-
-# Q: What can you say about this evidence? Interpret the coefficient of interest.
-
-# Q: What do you think will happen if we add covariates?
-
-summary(lm_robust(vote02 ~ treat_real + strata + vote00 + vote98 + age + female2, data = d_s1))
-
-
 #############################################
 
 # Just for performance purposes, we will use a smaller sample of the original data (just looking at one stratum):
@@ -102,34 +59,30 @@ d_sample <- d_s1 %>% filter(state == 1 & competiv == 1)
 
 #install.packages(MatchIt)
 library(MatchIt)
+library(optmatch)
 
-# The formula inside matchit() is the treatment (contact, which is different to the treatment assignment) as a function of the covariates. Remember to include the strata!
-# We will just use NN matching first, and exact match on strata.
-# Question: We don't include state here, why? 
+# The formula inside matchit() is the treatment (contact, which is different to the treatment assignment) as a function of the covariates. 
+# If we had different strata, you would need to add that, too.
+# We will just use Nearest Neighbor matching first.
 
-# Note that the arguments in the matchit() function are the formula (just like in lm(), I include all the covariates I want to adjust for)
+# Note that the arguments in the matchit() function are the formula (just like in lm(), I include all the covariates I want to control for)
 # I also give matchit the sample I want to use
 # The method then it's important. There are different kind of methods, but here we are using "nearest", which refers to nearest neighbor matching 
 # (looks for the closest neighbor in terms of propensity score and matches it to that!)
-# This is optional, but I'm asking the function to match exactly on strata (meaning, I can only find a treated unit for a control unit within the same strata)
+# This is optional, but I'm asking the function to match exactly on state (meaning, I can only find a treated unit for a control unit within the same state)
 # Finally, I'm also setting a caliper of 0.2. This means that, at most, treated and control matched units can be 0.2 units apart (this will reduce our sample size, but improve the matching)
 
 m1 <- matchit(contact ~ persons + vote00 + vote98 + newreg + age + female2, data = d_sample,
-              method = "nearest", exact = ~ strata, caliper = 0.2)
+              method = "nearest", exact = ~ state, caliper = 0.2)
 
-#m1$match.matrix stores the indexes (rows) for the matched control units (notice that they are stored as strings, so we need to transform them to numeric!)
-head(m1$match.matrix)
+# Let's check balance before and after matching (focus on the first three columns)
+summary(m1)
 
-#We will save both the treatment and control index: (Note: as.numeric(x) basically transforms a variable to numeric)
-t_id <- as.numeric(rownames(m1$match.matrix)) #We extract the rownames from the matrix, and convert it to numeric
-c_id <- as.numeric(m1$match.matrix)
-
-# Generate a data frame only with the matched units:
-d_m1 <- d_sample %>% slice(c(t_id, c_id)) # Slice subsets the data according to row numbers 
-# Q: How many observations should d_m1 have? Why?
+# Now, let's get our matched data:
+d_m1 <- match.data(m1)
 
 # Check whether it looks good:
-d_m1 %>% group_by(contact) %>% count() #Same number of treatment and controls!
+d_m1 %>% select(contact) %>% table() #Same number of treatment and controls!
 
 # Let's check for balance
 d_m1_bal <- d_m1 %>% select(contact, persons,vote00,vote98,newreg,age,female2)
@@ -146,7 +99,9 @@ summary(lm_robust(vote02 ~ contact, data = d_m1))
 
 # Finally, let's compare the previous results with simple OLS.
 # Q: Should we get the same results? Why or why not? (look both at the point estimate and the significance)
-
 summary(lm_robust(vote02 ~ contact + persons + vote00 + vote98 + newreg + age + female2, data = d_sample))
 
-# Q: Do you think the CIA holds in this case? Why or why not?
+###########################################################################
+#### EXERCISE #############################################################
+###########################################################################
+
