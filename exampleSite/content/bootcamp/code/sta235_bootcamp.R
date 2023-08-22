898c29a6963bc3176d3b9f3bddb0c8f8efd61c00
Magdalena Bennett <mai.bennett@gmail.com>
2023-08-22 10:07:00 -0500

    update

---
+++ exampleSite/content/bootcamp/code/sta235_bootcamp.R
@@ -0,0 +1,20 @@
+##############################################################
+### Title: Bootcamp example code
+### Author: Magdalena Bennett
+### Date Created: 08/23/2023
+### Last edit: [08/23/2023] - Created code
+##############################################################
+
+#Clear memory
+rm(list = ls())
+
+#Clear the console
+cat("\014")
+
+#Turn off scientific notation (turn back on with 0)
+options(scipen = 999)
+
+# Load packages
+library(tidyverse)
+
+# Load data
