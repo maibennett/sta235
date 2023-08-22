938fefa3a8dcbb29d9d3005bb95c299f4b226549
Magdalena Bennett <mai.bennett@gmail.com>
2023-08-22 10:18:33 -0500

    update

--- exampleSite/content/bootcamp/code/sta235_bootcamp.R
+++ exampleSite/content/bootcamp/code/sta235_bootcamp.R
@@ -15,6 +15,15 @@ cat("\014")
 options(scipen = 999)
 
 # Load packages
-library(tidyverse)
+library(tidyverse) #includes dplyr and ggplot2!
 
-# Load data
+# If there is a package you don't have installed, you can use install.packages("tidyverse")
+# Only run once! (no need to install packages every time you run your code)
+
+# Load data (this is loading data directly from Github)
+sales = read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/bootcamp/data/US_Regional_Sales_Data.csv")
+
+sales = sales %>% clean_names() %>% rename(sales_team_id = x_sales_team_id,
+                                           customer_id = x_customer_id,
+                                           store_id = x_store_id,
+                                           product_id = x_store_id)
