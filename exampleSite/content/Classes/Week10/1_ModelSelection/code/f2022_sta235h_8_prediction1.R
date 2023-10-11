37b5d42670be79e7120822941c7d454a955a6d59
Magdalena Bennett <mai.bennett@gmail.com>
2023-10-11 10:54:15 -0500

    update

---
+++ exampleSite/content/Classes/Week10/1_ModelSelection/code/f2022_sta235h_8_prediction1.R
@@ -0,0 +1,91 @@
+################################################################################
+### Title: "Week 10 - Prediction I"
+### Course: STA 235H
+### Semester: Fall 2022
+### Professor: Magdalena Bennett
+################################################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
+
+### Load libraries
+# If you don't have one of these packages installed already, you will need to 
+#run install.packages() line
+library(tidyverse)
+library(estimatr)
+library(modelr)
+library(caret)
+
+################################################################################
+################ Measuring churn ###############################################
+
+hbo <- read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week10/1_ModelSelection/data/hbomax.csv")
+
+head(hbo)
+
+# Divide data: 80% vs 20% split
+
+set.seed(100) #Always set seed for replication! (and make sure you are running an updated version of R!)
+
+n <- nrow(hbo) # Will tell us how many observations we have
+
+train <- sample(1:n, n*0.8) #randomly select 80% of the rows for our training sample
+
+# slice() selects rows from a dataset based on the row number.
+train.data <- hbo %>% slice(train) #use only the rows that were selected for training
+
+test.data <- hbo %>% slice(-train) #the rest are used for testing
+
+### Simple model
+lm_simple <- lm(logins ~ got + city, data = train.data) #Train the model on the TRAINING DATASET
+
+### Complex model
+lm_complex <- lm(logins ~ female + city + age + I(age^2) + got, data = train.data) #Train the model on the TRAINING DATASET
+
+
+# Estimate RMSE for these models on the TRAINING dataset:
+# For simple model:
+rmse(lm_simple, train.data) #rmse() in the `modelr` package takes model we are using and the data.
+
+# For complex model:
+rmse(lm_complex, train.data)
+
+# If we wanted to actually get the predictions, we could also do that with the following line:
+pred_complex_train <- lm_complex %>% predict(train.data) #We start with the model, and then use the function predict on the data we want.
+
+## Question: According to this, which model is better? Is this the comparison we want?
+
+# Estimate RMSE for these models on the TESTING dataset (THIS IS WHAT WE WANT TO ASSESS THE MODEL):
+# For simple model:
+rmse(lm_simple, test.data)
+
+# For complex model:
+rmse(lm_complex, test.data)
+
+## Question: Using this comparison, which model is better? Is this the comparison we want?
+
+###############################################################################
+#### Cross-validation
+###############################################################################
+
+# We will typically use 5 or 10-fold cross validation
+
+set.seed(100) # Set seed for replication!
+
+train.control <- trainControl(method = "cv", number = 10) #This is a function from the package caret. We are telling our data that we will use a cross validation approach (cv) with 10 folds (number). Use ?trainControl to see the different methods we could use!
+
+lm_simple_cv <- train(logins ~ got + city, data = hbo, method="lm",
+               trControl = train.control) #See that here (in the train function), we just pass all the data. The function will divide it in folds and do all that!
+
+lm_simple_cv
+
+
+lm_complex_cv <- train(logins ~ female + city + age + I(age^2) + got, data = hbo, method="lm",
+                       trControl = train.control) #See that here (in the train function), we just pass all the data. The function will divide it in folds and do all that!
+
+lm_complex_cv
+
+
+# Exercise: use the function `trainControl(method = "LOOCV")` to do a Leave One Out (LOO) cross validation. Do your results change? How? What are the advantages and disadvantages of using LOO?
---
+++ exampleSite/content/Classes/Week10/_index.en.md
@@ -0,0 +1,52 @@
+---
+title: Week 10 - 10/23
+weight: 55
+disableToc: true
+---
+
+## Date: Oct 23th - Oct 25th
+
+## What we will cover
+
+In this class, we wil switch gears and start with our prediction chapter. We’ll be talking about model checking, bias-variance tradeoff, cross-validation, and variable selection.
+
+## Recommended readings/videos
+
+- James, G. et al. (2021). "An Introduction to Statistical Learning with Applications in R" (ISLR). *Chapter 2.1.3 (pg. 24-26)*, *Chapter 2.2: 2.2.1 and 2.2.2 (pg. 29-36)*, and *Chapter 6: 6.1.2 (pg. 225-236 and 229-232)*. *Note: For ISLR readings, don't get caught up in the math.*
+
+- StatQuest. (2018). "Machine Learning Fundamentals: Bias and Variance".
+
+{{< youtube src="https://www.youtube.com/embed/EuBBz3bI-aA?controls=0" >}}
+
+- Ritvik Kharkar. (2020). "Cross-Validation: Data Science Concepts". *Video materials from ritvikmath*.
+
+{{< youtube src="https://www.youtube.com/embed/wjILv3-UGM8?controls=0" >}}
+
+- OpenIntroOrg. (2015). "Model Selection in Multiple Regression". *Video materials from OpenIntro*.
+
+{{< youtube src="https://www.youtube.com/embed/VB1qSwoF-l0?controls=0" >}}
+
+
+## Slides
+
+<!-- {{% button href="https://sta235.netlify.app/Classes/Week10/1_ModelSelection/f2022_sta235h_13_ModelSelectionI.html" icon="fas fa-external-link-alt" icon-position="right" %}}New window{{% /button %}} {{% button href="https://sta235.netlify.app/Classes/Week10/1_ModelSelection/f2022_sta235h_13_ModelSelectionI.pdf" icon="fas fa-file-pdf" icon-position="right" %}}Download{{% /button %}} 
+
+{{< slides src="https://sta235.netlify.app/Classes/Week10/1_ModelSelection/f2022_sta235h_13_ModelSelectionI.html" >}}
+ -->
+
+## Code
+
+<!-- Here is the R code we will review in class, with many additional questions! Remember to review it in detail after class <a onclick="ga('send', 'event', 'External-Link','click','code5','0','Link');" href="https://sta235.netlify.app/Classes/Week10/1_ModelSelection/code/f2022_sta235h_8_prediction1.R" target="_blank" class="btn btn-default">Download<i class="fas fa-code"></i></a>
+
+Check out the in-class activity we did for this week <a onclick="ga('send', 'event', 'External-Link','click','code10','0','Link');" href="https://sta235h.rocks/Week10" target="_blank" class="btn btn-default">Open<i class="fas  fa-external-link-alt"></i></a>
+
+(The answers for this are here: <a onclick="ga('send', 'event', 'External-Link','click','code10Answers','0','Link');" href="https://sta235h.rocks/Week10Answers" target="_blank" class="btn btn-default">Open<i class="fas  fa-external-link-alt"></i></a>) -->
+
+
+## Additional Materials
+
+To better understand the importance of the variance-bias concept, I find this video very insightful (watch it after we cover these concepts in class if you need better understanding of this topic!)
+
+- Ritvik Kharkar. (2020). "Bias-Variance tradeoff: Data Science Basics". *Video materials from ritvikmath*.
+
+{{< youtube src="https://www.youtube.com/embed/YIPsfEtJppE?controls=0" >}}
