09aa443a8c406259604456392894a66d7201f0bb
Magdalena Bennett <mai.bennett@gmail.com>
2023-10-11 11:50:56 -0500

    update

---
+++ exampleSite/content/Exams/Exams/STA235H_Midterm_Template.R
@@ -0,0 +1,23 @@
+######################################################################
+### Title: "Midterm"
+### Course: STA 235H
+### Semester: Fall 2023
+### Name: [INSERT YOUR NAME HERE]
+#######################################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
+
+### Load libraries
+library(tidyverse)
+library(modelsummary)
+library(estimatr)
+library(MatchIt)
+library(rdrobust)
+
+# Task 2
+
+
+# Task 3
