9379c4920f8c680a56e720b5f7aeba13a38dbd82
Magdalena Bennett <mai.bennett@gmail.com>
2023-10-31 15:54:04 -0500

    update

---
+++ exampleSite/content/Assignments/Homework/Homework6/_index.en.md
@@ -0,0 +1,55 @@
+---
+title: Homework 6
+weight: 25
+chapter: false
+---
+
+
+## Objective
+
+- Carry out a full prediction project by applying what we have learned about prediction in this class.
+
+	- Note: You can only use methods/packages that we have covered in this course.
+
+## How this works
+
+- You will choose a dataset out of the four (4) available ones, and compete with other students that have chosen the same one.
+
+- For each dataset, you will have to predict 2 outcomes: one continuous outcome and one binary outcome. You will use two (2) methods for each, and state which of the two is your preferred one.
+
+- Using each preferred method, the instruction team will run your code on a testing dataset with the same structure as the data provided and obtain the appropriate measures for accuracy.
+
+- For each outcome, we will rank the performance of the model for each student, and average their ranking. This will determine their score in the "performance" aspect (see evaluation below).
+
+## What you need to submit
+
+- A 2-page report (without counting images/tables) about your project with the following sections:
+
+	1) **Data description:** Brief description of the data (e.g. what is the data about, main variables you have, how many observations and variables; if you cleaned the data: how did you do it and how did it change from the original dataset?). Give the reader an idea of the data that you started with, and if you cleaned it, the data that you ended up with, so we know what you are working with.
+	2) **Task 1 - Continuous outcome:** Describe the two models you chose. Include a <u>brief</u> explanation of what each model does, and the main parameters you used for training the model (if you iterated through these, no need to describe the process, just the final parameters chosen). Show the appropriate results (e.g. tuning parameters, plots, measures of accuracy, etc.). <u>Be clear about which of the two is your preferred model</u>.
+	3) **Task 2 - Binary outcome:** Describe the two models you chose. Include a <u>brief</u> explanation of what each model does (if you didn't describe it in the previous section), and the main parameters you used for training the model (if you iterated through these, no need to describe the process, just the final parameters chosen). Show the appropriate results (e.g. tuning parameters, plots, measures of accuracy, etc.). <u>Be clear about which of the two is your preferred model</u>.
+
+- The report needs to have a title (you can keep it classic or be creative) and clearly indicate the different sections/subsections. All tables and plots need to be place at the end of the document (they don't count in the 2-page limit), and have appropriate descriptive captions (numbered so you can reference them in the text).
+
+- Use standard margins and a font size of at least 11pt.
+
+- You will also need to submit an R script with the code and an RData file with your model (see example here).
+
+	- Make sure that your code runs. If it doesn't, you will be assigned the last place in the ranking.
+
+## Evaluation
+
+- Homework assignments will be graded according to the following items:
+
+	- **Style**: The report follows the styling guidelines.
+	- **Clarity**: Descriptions and explanations are clear and accurate.
+	- **Model training**: Training parameters are appropriate for the task at hand.
+	- **Results**: Presentation of results.
+	- **Model evaluation**: Selection of the best performing model.
+	- **Code**: Whether the code is correct or not for the task.
+	- **Model performance**: Average ranking of the performance.
+
+- <u>Without considering performance</u>, the maximum grade is 94 points. The last 6 points will be awarded based on performance, where the student with the best performing models (on average) will receive 6 points, and the student with the lowest performance model (or a code that doesn't allow estimating the performance) will receive 0 points.
+
+
+
---
+++ exampleSite/content/Classes/Week12/1_DecisionTrees/code/f2023_sta235h_12_decisiontrees.R
@@ -0,0 +1,112 @@
+################################################################################
+### Title: "Week 12 - Decision Trees"
+### Course: STA 235H
+### Semester: Fall 2022
+### Professor: Magdalena Bennett
+################################################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
+
+### Load libraries
+# If you don't have one of these packages installed already, you will need to 
+#run install.packages() line
+library(tidyverse)
+library(estimatr)
+library(modelr)
+library(caret)
+library(rpart)
+library(rattle)
+
+################################################################################
+################ Measuring churn ###############################################
+
+hbo = read.csv("https://raw.githubusercontent.com/maibennett/sta235/main/exampleSite/content/Classes/Week12/1_DecisionTrees/data/hbomax2.csv")
+
+head(hbo)
+
+#In this case, I have already provided the training dataset:
+train.data = hbo %>% filter(train==1)
+test.data = hbo %>% filter(train==0)
+
+
+###############################################################################
+##### Classification tree
+###############################################################################
+
+set.seed(100)
+
+train.control = trainControl(method = "cv", number = 10) #set up a 10-fold cv
+
+ct = train(factor(unsubscribe) ~ . - id, data = train.data,
+           method = "rpart", 
+           tuneLength = 15, # This tells the model to choose 15 values of `cp` and test them (this is a good starting point, but tuneGrid is better!)
+           trControl = train.control)
+
+# What is our best complexity parameter in this case?
+ct$bestTune
+
+# We can also see the plot here:
+plot(ct)
+
+# Let's see the final classification tree:
+ct$finalModel
+
+# Question: Which one is your most predictive covariate here?
+
+#We can also plot this in a prettier way using the rattle package:
+fancyRpartPlot(ct$finalModel, caption = "Classification Tree")
+
+# Question: How would you classify someone who hasn't watched Succession and has more than one login in the previous week?
+
+# Finally, we can also estimate the accuracy of this model, the same way we did it for our classification task in ridge or lasso:
+
+pred.unsubscribe = ct %>% predict(test.data)
+
+mean(pred.unsubscribe == test.data$unsubscribe)
+
+# Exercise: Instead of using tuneLength = 15, use tuneGrid = expand.grid(cp = seq(0,0.015, length = 50)) and see what you get!
+
+############################################
+#### Regression Tree
+############################################
+
+# Let's now complete the same task as before, but using logins as our outcome. Also, let's add some more stuff!
+set.seed(100)
+
+train.control = trainControl(method = "cv", number = 10) #set up a 10-fold cv
+
+rt = train(logins ~ . - unsubscribe - id, data = train.data,
+            method = "rpart", 
+            tuneGrid = expand.grid(cp = seq(0, 0.01, length = 50)), # This tells the model to test 50 (equally-spaced) values of cp between 0 and 0.01
+            trControl = train.control)
+
+# What is our best complexity parameter in this case?
+rt$bestTune
+
+# We can also see the plot here:
+plot(rt)
+
+# Let's see our regression tree
+fancyRpartPlot(rt$finalModel, caption = "Regression Tree")
+
+# Now, we can estimate the RMSE of this model as well:
+
+rmse(rt, test.data)
+
+# Exercise: Compare this model with lasso or ridge regression from the previous class. Which one does better?
+
+# Finally, we can also add other stopping conditions to make sure our leaves don't get too small:
+
+set.seed(100)
+
+rt = train(logins ~ . - unsubscribe - id, data = train.data,
+            method = "rpart", 
+            tuneGrid = expand.grid(cp = seq(0, 0.01, length = 70)), # Just for fun, I changed the length of this vector to 70
+            trControl = train.control,
+            control = rpart.control(minsplit = 15)) # This tells the model that there needs to be at least 15 observations in each leave for it to attempt a split!
+
+# What is our best complexity parameter in this case?
+rt$bestTune
