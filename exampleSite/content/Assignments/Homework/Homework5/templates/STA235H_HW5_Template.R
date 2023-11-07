5241e57382ca027cdd54cbde43c5055007342c15
Magdalena Bennett <mai.bennett@gmail.com>
2023-11-07 16:16:55 -0600

    Create STA235H_HW5_Template.R

---
+++ exampleSite/content/Assignments/Homework/Homework5/templates/STA235H_HW5_Template.R
@@ -0,0 +1,26 @@
+######################################################################
+### Title: "Homework 5"
+### Course: STA 235H
+### Semester: Fall 2023
+### Name: [INSERT YOUR NAME HERE]
+#######################################################################
+
+# Clears memory
+rm(list = ls())
+# Clears console
+cat("\014")
+
+### Load libraries
+# [LOAD PACKAGES HERE]
+
+# Task 2 - 
+
+
+# Q2.1
+
+
+# Task 3 - 
+
+
+
+# Q3.1
